229e2c91434d2c7304ecbbdf28c613a6ad402d7d
huangzijian <huangzijian@lamdaer.com>
2020-05-04 06:57:14 +0800

    接口地址

---
+++ src/main/java/com/lamdaer/polyv/live/constant/ApiConstants.java
@@ -0,0 +1,15 @@
+package com.lamdaer.polyv.live.constant;
+
+/**
+ * 接口地址
+ *
+ * @author lamdaer
+ * createTime 2020/5/4
+ */
+public class ApiConstants {
+
+    /**
+     * 修改直播频道分类名称
+     */
+    public static final String CHANGE_CHANNEL_CLASSIFICATION_NAME = "http://api.polyv.net/live/v3/user/category/update-name";
+}
