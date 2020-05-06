49ff720c75ff0c355ad784b47e8d380e71b96827
huangzijian <huangzijian@lamdaer.com>
2020-05-06 13:29:00 +0800

    增加错误代码及默认值枚举类

---
+++ src/main/java/com/lamdaer/polyv/live/enumeration/ErrorCodeEnum.java
@@ -0,0 +1,57 @@
+package com.lamdaer.polyv.live.enumeration;
+
+/**
+ * 错误代码及默认值
+ *
+ * @author lamdaer
+ * createTime 2020/5/4
+ */
+public enum ErrorCodeEnum {
+    DEFAULT(-99999, "int类型默认值"),
+    ;
+
+    /**
+     * <pre>
+     * 字段名：错误代码/默认值
+     * 变量名：code
+     * 是否必填：是
+     * </pre>
+     */
+    private int code;
+
+    /**
+     * <pre>
+     * 字段名：提示信息
+     * 变量名：msg
+     * 是否必填：是
+     * </pre>
+     */
+    private String msg;
+
+    ErrorCodeEnum(int code, String msg) {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorCodeEnum{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
