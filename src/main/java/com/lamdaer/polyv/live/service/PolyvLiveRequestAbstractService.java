a2fbd3f1798e07ba58a45b72d7d3c2b4813a16ab
huangzijian <huangzijian@lamdaer.com>
2020-05-29 20:32:54 +0800

    增加日志配置、异常捕获

--- src/main/java/com/lamdaer/polyv/live/service/PolyvLiveRequestAbstractService.java
+++ src/main/java/com/lamdaer/polyv/live/service/PolyvLiveRequestAbstractService.java
@@ -6,6 +6,9 @@ import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONUtil;
 import com.lamdaer.polyv.live.bean.result.PolyvLiveCommonResult;
+import com.lamdaer.polyv.live.enumeration.ErrorCodeEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -16,6 +19,8 @@ import java.util.Map;
  * createTime 2020/05/03
  */
 public abstract class PolyvLiveRequestAbstractService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PolyvLiveRequestAbstractService.class);
 
     /**
      * get 请求
@@ -47,11 +52,20 @@ public abstract class PolyvLiveRequestAbstractService {
      * @param result 原始返回信息
      * @return 处理后的返回信息
      */
-    private PolyvLiveCommonResult getPlLiveCommonResult(String result) {
-        JSON jsonResult = JSONUtil.parse(result);
-        int code = (int) jsonResult.getByPath("code");
-        String status = (String) jsonResult.getByPath("status");
-        String message = (String) jsonResult.getByPath("message");
+    public PolyvLiveCommonResult getPlLiveCommonResult(String result) {
+        JSON jsonResult = null;
+        int code = 0;
+        String status = null;
+        String message = null;
+        try {
+            jsonResult = JSONUtil.parse(result);
+            code = (int) jsonResult.getByPath("code");
+            status = (String) jsonResult.getByPath("status");
+            message = (String) jsonResult.getByPath("message");
+        } catch (Exception e) {
+            LOG.error("json parse error.");
+            return new PolyvLiveCommonResult(ErrorCodeEnum.JSON_PARSE_ERROR.getCode(), "error", ErrorCodeEnum.JSON_PARSE_ERROR.getMsg(), null);
+        }
         if (HttpStatus.HTTP_OK == code) {
             return new PolyvLiveCommonResult(code, status, message, jsonResult.getByPath("data"));
         }
